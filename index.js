9d929e639681fedeaa6ba55699464338bc33ea35
Drew Wyatt <drew.wyatt@bottlerocketstudios.com>
2016-09-18 22:53:19 -0500

    WIP

--- index.js
+++ index.js
@@ -18,11 +18,34 @@ const screen = blessed.screen({
 
 screen.title = 'reddit-cli';
 
+function onSelect(box, screen) {
+	return function(something) {
+		box.setContent('boom');
+		screen.render();
+	}
+}
+
+const box = blessed.box({
+	top: '0',
+    right: '0',
+    width: '50%',
+    height: '100%',
+    style: {
+        // fg: 'white',
+        // bg: 'magenta',
+        border: {
+            fg: '#f0f0f0'
+        }
+    },
+	content: 'Post content here.'
+});
+
+
 const list = blessed.list({
     top: '0',
     left: '0',
     width: '50%',
-    height: '50%',
+    height: '100%',
     style: {
         // fg: 'white',
         // bg: 'magenta',
@@ -39,17 +62,39 @@ const list = blessed.list({
             bold: true
         }
     },
-    keys: true
+    keys: true,
+	content: 'Fetching submissions...'
 });
 
-r.getHot().map(submissionToListablePost).then(list.setItems.bind(list));
+var itemMap = [];
 
-function submissionToListablePost(submission) {
-    return [`[${submission.id}]`, submission.title, `(by /u/${submission.author.name})`].join(' ');
-}
-
+list.on('select', function(el, selected) {
+	const id = el.getText().split(':')[0];
+	// const foo = itemMap.find(i => i.id === id);
+	box.setContent(JSON.stringify(itemMap));
+	screen.render();
+})
 
 screen.append(list);
+screen.append(box);
+
+r.getHot().map(submissionToListablePost).then(setItems).then(updateList(list, screen));
+
+function setItems(items) {
+	itemMap = items;
+	return Promise.resolve(items);
+}
+
+function updateList(list, screen) {
+	return function (items) {
+		list.setItems(items);
+		screen.render();
+	}
+}
+
+function submissionToListablePost(submission) {
+    return [`${submission.id}:`, submission.title, `(by /u/${submission.author.name})`].join(' ');
+}
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
